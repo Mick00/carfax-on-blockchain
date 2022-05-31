b535826ef95db4fd472634fdf5da25bae4af0191
mOGr <gagnermarcolivier@gmail.com>
2022-05-31 09:44:37 -0400

    Added registrars tracking and validation.

--- packages/hardhat-ts/contracts/Contributors.sol
+++ packages/hardhat-ts/contracts/Contributors.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity 0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -10,6 +10,8 @@ contract Contributors is ERC721URIStorage, Ownable {
   mapping(address => mapping(address => string)) private waitingForConfirmation;
   mapping(uint256 => address) private tokenToContributor;
 
+  mapping(address => bool) private registrars;
+
   //id count.
   uint256 private tokenIds;
 
@@ -18,15 +20,17 @@ contract Contributors is ERC721URIStorage, Ownable {
   event ConfirmedRegistration(address contributor, uint256 tokenId);
   event RemovedRegistrator(address registrator, uint256 tokenId);
 
+  modifier isRegistrar() {
+    require(registrars[msg.sender], "Caller is not a registrar");
+    _;
+  }
+
   //Gas to deploy contract : 3233690
   constructor() ERC721("CarFaxContributor", "CFC") {}
 
   //Gas without registrar validation : 48671
-  function register(string memory _contributorHash, address _contributor) external {
+  function register(string memory _contributorHash, address _contributor) external isRegistrar {
     address registrar = msg.sender;
-    // caller must be a valid registrar
-
-    // if confirmation already exist?
 
     waitingForConfirmation[registrar][_contributor] = _contributorHash;
 
@@ -69,6 +73,14 @@ contract Contributors is ERC721URIStorage, Ownable {
     address registrator = super.ownerOf(_tokenId);
     super._burn(_tokenId);
     emit RemovedRegistrator(registrator, _tokenId);
+  }
+
+  function addRegistrar(address _registrar) external onlyOwner {
+    registrars[_registrar] = true;
+  }
+
+  function removeRegistrar(address _registrar) external onlyOwner {
+    delete registrars[_registrar];
   }
 
   function _isHashEmpty(string memory hash) private pure returns (bool) {
