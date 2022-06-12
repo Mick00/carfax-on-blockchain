1b1d8cd95e60df8741215e3c9b82e4f379e78447
lokman <lokman@iptoki.com>
2022-06-11 21:06:12 -0400

    after merge
    Merged with dashboard

--- packages/registrar-frontend/pages/register.tsx
+++ packages/registrar-frontend/pages/register.tsx
@@ -1,13 +1,7 @@
-export default function FirstPost(props) {
+export default function FirstPost() {
     return (
     <>
     <h1>test 1</h1>
-            <h2>{props.id}</h2>
     </>
     );
 }
-
-export async function getServerSideProps({params,req,res,query,preview,previewData,resolvedUrl,locale,locales,defaultLocale}) {
-    console.log('Logging : exec here '+res);
-    return {props: {id:"one"}};
-  }
