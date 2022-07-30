ab730c11675337e098ac438af5336cd5086c8a43
Mick00 <20918417+Mick00@users.noreply.github.com>
2022-07-30 15:09:08 -0400

    add hash to car search

---
+++ packages/protocol-frontend/src/components/IPFSFile.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Box, Button, IconButton, Link, Menu, MenuItem, Stack } from "@mui/material";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+const PLACEHOLDER = "{CID}"
+
+const gatewaysList = [
+  {
+    label: "ipfs.io",
+    url: `https://ipfs.io/ipfs/${PLACEHOLDER}`
+  }, {
+    label: "dweb.link",
+    url: `https://${PLACEHOLDER}.ipfs.dweb.link`
+  }
+]
+
+interface IPFSFileProp extends React.PropsWithChildren{
+  hash: string
+}
+
+export default function IPFSFile(props: IPFSFileProp){
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (anchorEl) {
+      setAnchorEl(null);
+    } else {
+      setAnchorEl(event.currentTarget);
+    }
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Stack direction={"row"}>
+      {props.children}
+      <IconButton size={"small"} onClick={handleClick}><VisibilityIcon/></IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        {gatewaysList.map((gateway) =>(
+          <MenuItem>
+            <Link
+              href={gateway.url.replaceAll(PLACEHOLDER, props.hash)}
+              target={"_blank"}
+              rel={"nofollow noopener noreferrer"}
+            >
+              {gateway.label}
+            </Link>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Stack>
+  )
+}
--- packages/protocol-frontend/src/panes/cars/Search.tsx
+++ packages/protocol-frontend/src/panes/cars/Search.tsx
@@ -7,19 +7,27 @@ import { isValidVin } from "../../components/inputs/rules";
 import React, { useState } from "react";
 import { BigNumber } from "ethers";
 import { formatUnits } from "ethers/lib/utils";
+import IPFSFile from "../../components/IPFSFile";
 
 export default function CarSearch() {
   const { cars, canRead } = useCOBApi();
   const { handleSubmit, control } = useForm();
-  const [searchVin, setSearchVin] = useState("");
+  const [vin, setVin] = useState("");
   const [carId, setCarId] = useState(0);
 
-  const onSubmit = (data: any) => setSearchVin(data.vin);
+  const onSubmit = (data: any) => setVin(data.vin);
 
-  const {data: car, isLoading} = useQuery(["cars", searchVin], () =>
-    canRead? cars().getOdometerFromSerialNumber(searchVin): Promise.resolve(BigNumber.from("0")),
-    { enabled: canRead && Boolean(searchVin)}
+  const {data: odometer, isLoading: isLoadingOdometer } = useQuery(["cars", "odometer", vin], () =>
+    cars().getOdometerFromSerialNumber(vin),
+    { enabled: canRead && Boolean(vin)}
   )
+
+  const {data: uri, isLoading: isLoadingURI} = useQuery(["cars", "URI", vin], () =>
+      cars().getTokenURIFromSerialNumber(vin),
+    { enabled: canRead && Boolean(vin)}
+  )
+
+  const isLoading = isLoadingOdometer || isLoadingURI;
 
   return (
     <Box>
@@ -29,9 +37,11 @@ export default function CarSearch() {
         <Button onClick={handleSubmit(onSubmit)} variant={"contained"}>Search</Button>
       </form>
       {isLoading && (<Typography>Loading...</Typography>)}
-      {car && (
-        <Box>
-          <Typography>Odometer: {formatUnits(car.toString(), 4)}</Typography>
+      {!uri && !odometer && (<Typography>No cars found</Typography>)}
+      {odometer && (
+        <Box mt={1}>
+          <IPFSFile hash={uri??""}><Typography>Uri: {uri}</Typography></IPFSFile>
+          <Typography>Odometer: {formatUnits(odometer.toString(), 4)}</Typography>
         </Box>
       )}
     </Box>
